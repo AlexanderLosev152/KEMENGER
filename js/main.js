6f9d7e11fab3da21aacd00e7dda2a1bf59d57601
AlexanderLosev152 <losev.pavlovo@mail.ru>
2023-01-08 14:12:07 +0300

    14.12

--- js/main.js
+++ js/main.js
@@ -7,45 +7,11 @@
         },
         mousewhell: true,
         keyboard: true,
+        grabCursor:true,
       });
 
 
 
-      // function readMore() {
-      //   var dots = document.getElementById("dots")
-      //   var more = document.getElementById("more")
-      //   var btn = document.getElementById("btn")
-
-      //   if(dots.style.display === "none" ) {
-      //     dots.style.display="inline"
-      //     btn.innerHTML="Подробнее..."
-      //     more.style.display="none"
-      //   } else {
-      //     dots.style.display="none"
-      //     btn.innerHTML="Скрыть.."
-      //     more.style.display="inline"
-      //   }
-      // }
-
-    
-      // for(btn of btns) {
-      //   btn.addEventListener('click',  function () {
-      //     let card = this.closest('swiper-slide');
-      //     let dots = card.querySelectorAll('.dots');
-      //     let more = card.querySelectorAll('.more');
-
-      //     if(dots.style.display === 'none') {
-      //       dots.style.display = 'inline';
-      //       more.style.display = 'none';
-      //       this.textContent = 'Подробнее';
-      //     }else {
-      //       dots.style.display = 'none';
-      //       more.style.display = 'inline';
-      //       this.textContent = 'Скрыть';
-      //     }
-        
-      //   })
-      // }
 
   function readMore(dotsId, moreId, btnId) {
   console.log('asdasdasdasd')
