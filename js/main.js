44179ea956042ee9fab1caecd8811b2a3ca5f0a2
AlexanderLosev152 <losev.pavlovo@mail.ru>
2023-01-08 19:20:48 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-      var swiper = new Swiper(".mySwiper", {
+    //  модальное окно
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
+    // span.onclick = function () {
+    //   modal.style.display = "none";
+    // } 
+     
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+    //  ----------------------
+    
+    
+
+    // swiper
+
+     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
         spaceBetween: 30,
         navigation: {
