cf651fcee9fced0f993cf8fb962640ddd5cf5802
AlexanderLosev152 <losev.pavlovo@mail.ru>
2023-01-08 23:30:33 +0300

    modalClosed

--- js/main.js
+++ js/main.js
@@ -18,7 +18,22 @@
 
     //  ----------------------
     
+    // плавный скрол
+
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        })
+      })
+    }
     
+    // -------------------------
 
     // swiper
 
